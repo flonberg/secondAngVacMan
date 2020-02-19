cef64f10ae6bb368f2df6cd49ac0983c41f0f41d
Franklin Lonberg <franklinlonberg@franklins-imac.partners.org>
2020-02-19 16:43:19 -0500

    fix firt week

--- src/app/month-view/month-view.component.ts
+++ src/app/month-view/month-view.component.ts
@@ -25,7 +25,7 @@ export class MonthViewComponent implements OnInit {
   ngOnInit() {
     this.daysS = Array(Array());
     var tmpDate = new Date();
-    var date = new Date('2019-03-05');          // set the date, done by queryParam
+    var date = new Date('2019-02-05');          // set the date, done by queryParam
     var firstDayOfShownMonth = new Date(date.getFullYear(), date.getMonth(), 1);
     var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
     var dowFD = firstDayOfShownMonth.getDay();            // 5 for Thursday
@@ -46,7 +46,7 @@ export class MonthViewComponent implements OnInit {
         this.daysS[0][i].date = new Date(lastDayPrevMonth.getFullYear(), lastDayPrevMonth.getMonth(), dayOnCal);  // set date in array
       dayOnCal++; 
       if (i > 0 )  {                            // go to next day
-        tmpDate =  this.daysS[0][i-1].date
+        tmpDate =  new Date(this.daysS[0][i-1].date.getFullYear(), this.daysS[0][i-1].date.getMonth(), this.daysS[0][i-1].date.getDate())
         tmpDate.setDate(tmpDate.getDate() + 1); // increment the date
          this.daysS[0][i].date = tmpDate;
       }
