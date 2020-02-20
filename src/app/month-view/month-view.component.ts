7932d60da213179862fa87aba5be0c6f15b111df
Franklin Lonberg <franklinlonberg@franklins-imac.partners.org>
2020-02-20 15:09:05 -0500

    next month implemented

--- src/app/month-view/month-view.component.ts
+++ src/app/month-view/month-view.component.ts
@@ -24,23 +24,35 @@ export class MonthViewComponent implements OnInit {
   prevMonthLastDays:[];
   daysS: dateBox[][];
   datStruc: any;
+  monthName: string;
+  date: Date;
+  monthNumber:number;
   constructor(private datePipe: DatePipe) { }
 
-  ngOnInit() {
+  ngOnInit(){
+    this.nextMonth(0);
+    this.monthNumber = 0;
+  }
+  nextMonth(nn)
+  {
     this.daysS = Array(Array());
     var tmpDate = new Date();
-    var date = new Date('2020-11-05');                                                        // set the date, done by queryParam
-    var firstDayOfShownMonth = new Date(date.getFullYear(), date.getMonth(), 1);              
-    var lastDayOfShownMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+    this.date = new Date('2020-11-28'); 
+    this.monthNumber += nn;
+    if (this.monthNumber > 0)
+    this.date = new Date(this.date.setMonth(this.date.getMonth()+ this.monthNumber));  
+    this.monthName = this.datePipe.transform(this.date, 'MMMM');                                                  // set the date, done by queryParam
+    var firstDayOfShownMonth = new Date(this.date.getFullYear(), this.date.getMonth(), 1);              
+    var lastDayOfShownMonth = new Date(this.date.getFullYear(), this.date.getMonth() + 1, 0);
     var dowFD = firstDayOfShownMonth.getDay();                                                // det dayOfWeek e.g. 5 for Friday, 0 = Sunday
     var n = 0;
     this.daysS = Array(Array());                                                              // create 2D array hold days  [week][day]
     console.log("fd  " + dowFD);
    // date.setMonth(date.getMonth() - 1);
-    var lastDayPrevMonth = new Date(date.getFullYear(), date.getMonth(), 0);                
+    var lastDayPrevMonth = new Date(this.date.getFullYear(), this.date.getMonth(), 0);                
     var lastDayNum = +this.datePipe.transform(lastDayPrevMonth,'d');                          //  e.g. for Jan 31  ->  31
     var firstDayOnCal = lastDayNum - (dowFD -2);   // get dayNum of first Monday on Cal       //  det how many days from prev. month we need. e.g Thurs we need 5 - 2 = 3
-    /////////////////            make days of first week      \\\\\\\\\\\\\\\\\\\
+    /////////////////            make days of first week                                        \\\\\\\\\\\\\\\\\\\
     if (dowFD > 0 && dowFD < 6){                                                               // if the firstDayOfMonth is NOT Sat or Sun  
       for (let i = 0;  i < 5; i++){                                                              // make the 5 days of the first week;
         if (!this.daysS[0])                                                                     //  if array row has not been defined
@@ -60,12 +72,13 @@ export class MonthViewComponent implements OnInit {
           firstDayOnCal = 1;                                                                    // go to 1, for the first day of monthShown 
       }
     }
+    /////////////         take care of months which start on Sat or Sun                       \\\\\\\\\\\\\\\\\\\\\\\\\\\\
     if (dowFD == 6)
-      tmpDate =  new Date(date.getFullYear(), date.getMonth(), 2);                            // if firstDayOfMonth is Sat. increment by 2 days
+      tmpDate =  new Date(this.date.getFullYear(), this.date.getMonth(), 2);                            // if firstDayOfMonth is Sat. increment by 2 days
     if (dowFD == 0)
-      tmpDate =  new Date(date.getFullYear(), date.getMonth(), 1);                            // if firstDayOfMonth is Sub. increment by 1 day
+      tmpDate =  new Date(this.date.getFullYear(), this.date.getMonth(), 1);                            // if firstDayOfMonth is Sub. increment by 1 day  
     ///////////  make the suceeding weeks  \\\\\\\\\\\\\\\\\\\\\\\\\\ 
-    for (let i=1; i < 5; i++){                                                                // max of 4 more weeks in calendar
+    for (let i=1; i < 6; i++){                                                                // max of 4 more weeks in calendar
         for (let j= 0; j < 5; j++) {                                                           // the days of each week
           tmpDate.setDate(tmpDate.getDate() + 1);                                             // increment the date
             let dayNum = tmpDate.getDay();                                                    // get dayNum of week  0 = Sunday  
@@ -74,15 +87,18 @@ export class MonthViewComponent implements OnInit {
             if (!this.daysS[i])                                                                     //  if array row has not been defined
               this.daysS[i] = Array();  
               this.daysS[i][j] = <dateBox>{};                                                         // define an instance of the daysS interface
-              this.daysS[i][j].date = new Date(tmpDate.getFullYear(), tmpDate.getMonth(), tmpDate.getDate());    
-           
+              this.daysS[i][j].date = new Date(tmpDate.getFullYear(), tmpDate.getMonth(), tmpDate.getDate());    // put date in daysS dataStructure.
+              this.daysS[i][j].dayNumber = tmpDate.getDate();
         }
-
     }
     console.log("i is "   )
   }
-
 }
+
+   
+
+
+
 
 
 
