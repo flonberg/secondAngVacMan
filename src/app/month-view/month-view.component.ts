3c5c5f90437f59e72357038560e77916f7b99b59
Franklin Lonberg <flonberg@gmail.com>
2020-10-13 17:50:32 -0400

    cal working thru April

--- src/app/month-view/month-view.component.ts
+++ src/app/month-view/month-view.component.ts
@@ -165,90 +165,118 @@ export class MonthViewComponent implements OnInit {
   }
   nextMonth(nn)
   {
-    this.daysS = Array(Array());                                     // make array to hold daysS structures
-    var tmpDate = new Date();                                       // this is the date which will be incremented
-    this.date = new Date();                                         //  this will be set to today in production
+    this.daysS = Array(Array());                                                      // make array to hold daysS structures
+    var tmpDate = new Date();                                                         // this is the date which will be incremented
+    this.date = new Date();                                                           //  today's date
     this.baseDate = new Date('2010-01-01');
-
-    this.monthNumber += nn;                                        // nn will be either +1 of -1 to go forward or bacf
-    if (nn != 0)                                                  // if date has been changed by button  
+    this.monthNumber += nn;                                                           // nn will be either +1 of -1 to go forward or bacf
+    if (nn != 0)                                                                      // if date has been changed by button  
       this.date = new Date(this.date.setMonth(this.date.getMonth()+ this.monthNumber));     // make the new date
     this.monthName = this.datePipe.transform(this.date, 'MMMM-yyyy');                       // used for the caption on the calendar 
     var firstDayOfShownMonth = new Date(this.date.getFullYear(), this.date.getMonth(), 1);   
     var firstDateOnCalendar = new Date(this.date.getFullYear(), this.date.getMonth(), 1);   
     var monthShowNumber = this.date.getMonth();                                              // use to grey out days NOT in monthShown
     const dowFD = firstDayOfShownMonth.getDay();                                             // det dayOfWeek e.g. 5 for Friday, 0 = Sunday, 1=Monday ...of 
-    var lastDayPrevMonth = new Date(this.date.getFullYear(), this.date.getMonth(), 0);                
-    var lastDayNum = +this.datePipe.transform(lastDayPrevMonth,'d');     //  e.g. for March   ->  31
+    var lastDayPrevMonth = new Date(this.date.getFullYear(), this.date.getMonth(), 0);    
+    var lastDayNumPrevMonth = lastDayPrevMonth.getDay();            
+    var lastDayNum = +this.datePipe.transform(lastDayPrevMonth,'d');                    //  e.g. for March   ->  31
+    console.log('182 lastDayPreMonth is %o lastDayNum is %o lastDayNumPrevMonth is %o ', lastDayPrevMonth, lastDayNum, lastDayNumPrevMonth)
+    if (lastDayNumPrevMonth > 0 && lastDayNumPrevMonth < 6){
+      for(var i = 1; i < lastDayNumPrevMonth; i++ ){
+            lastDayPrevMonth.setDate(lastDayPrevMonth.getDate() -1);
+          }
+        }
+    else  if (lastDayNumPrevMonth == 6)
+      lastDayPrevMonth.setDate(lastDayPrevMonth.getDate() + 2 );
+    else  if (lastDayNumPrevMonth == 0)
+      lastDayPrevMonth.setDate(lastDayPrevMonth.getDate() + 1 );
+      firstDateOnCalendar = lastDayPrevMonth;
+   
     //////////  use firstDayOnCal as dateSince to make array of physicsDuties     \\\\\\\\\\\\\\\\\
-    var firstDayOnCal = lastDayNum - (dowFD -2);      // get dayNum of first Monday on Cal       //  E.g. April 1 is Wed. to firstDayShown is March 29, so firstDanOnCal = 29
-    if (dowFD == 1)
+   // var firstDayOnCal = lastDayNum - (dowFD -2);     // get dayNum of first Monday on Cal       //  E.g. April 1 is Wed. to firstDayShown is March 29, so firstDanOnCal = 29
+    var firstDayOnCal = firstDateOnCalendar.getDate();
+    console.log('184 firstDayOnCal is ' + firstDayOnCal + 'lastDayNum is ' + lastDayNum + ' dowFD is ' + dowFD);
+    if (dowFD == 1)                                                                 // DayOfWeek of first day on Calendar is Monday, then the firstDate on cal is the '1' of month
       firstDayOnCal = 1;
     /************  make the first day shown on the calendar  **********************/
-    if (dowFD !== 6 && dowFD !== 0)
+    /*
+    if (dowFD !== 6 && dowFD !== 0)                                                 // first day of the month is a weekDay
       firstDateOnCalendar.setDate(firstDateOnCalendar.getDate() - (dowFD - 1 ));              // first of Month is a WeekDay so need to step back to Monday
     if (dowFD === 6 )
       firstDateOnCalendar.setDate(firstDateOnCalendar.getDate()+ 2 );                         // first of Month is Saturday so need to step forward 2 days to Monday
     if (dowFD === 0 )
       firstDateOnCalendar.setDate(firstDateOnCalendar.getDate()+ 1 );                         // first of Month is Sunday so need to step forward 1 day1 to Monday
-    this.startDateForGettingDataString = this.datePipe.transform(firstDateOnCalendar, 'yyyy-MM-dd');
+    */  
+    var GdayNumber = firstDateOnCalendar.getDay();
+    console.log("195 GdayNumber is " + GdayNumber);  
+    
+      this.startDateForGettingDataString = this.datePipe.transform(firstDateOnCalendar, 'yyyy-MM-dd');
     /////////////////            make days of first week                                        \\\\\\\\\\\\\\\\\\\
     var startDateForGettingData = new Date()                                         // define a date to set in the below loop
-    if (dowFD > 1 && dowFD < 6){                                                     // if the firstDayOfMonth is NOT Sat or Sun  
-      for (let i = 0;  i < 5; i++){                                                  // make the 5 days of the first week;
+    if (dowFD > 0 && dowFD < 6)                                                     // if the firstDayOfMonth is NOT Sat or Sun  
+      { 
+      for (let i = 0;  i < 5; i++)                                                 // make the 5 days of the first week;
+        {
         if (!this.daysS[0])                                                          //  if array row has not been defined
-          this.daysS[0] = Array();                                                   // define the array for the Week
-        this.daysS[0][i] = <dateBox>{};                                              // define an instance of the daysS interface
-        this.daysS[0][i].dayNumber = firstDayOnCal;                                  // set dayNumber element of interface
-       /************      first Day of first week **********************/
-        if ( i == 0){
-          this.daysS[0][i].date = new Date(lastDayPrevMonth.getFullYear(), lastDayPrevMonth.getMonth(), firstDayOnCal);  // set first date on Calendar in array
-         tmpDate = new Date(lastDayPrevMonth.getFullYear(), lastDayPrevMonth.getMonth(), firstDayOnCal);  // set first date on Calendar in array
-          if (dowFD == 1)
-            this.daysS[0][i].date = firstDateOnCalendar;
-          startDateForGettingData = this.daysS[0][i].date;
-          if (dowFD == 1 || dowFD == 6){
-            startDateForGettingData = firstDayOfShownMonth; 
-          }                                            
-          this.daysS[0][i].isInCurrMonth = tmpDate.getMonth() == monthShowNumber ? "inMonth" : "outMonth";
+            this.daysS[0] = Array();                                                   // define the array for the Week
+          this.daysS[0][i] = <dateBox>{};                                              // define an instance of the daysS interface
+          this.daysS[0][i].dayNumber = firstDayOnCal;                                  // set dayNumber element of interface
+        /************      first Day of first week **********************/
+          if ( i == 0)
+          {
+            this.daysS[0][i].date = new Date(lastDayPrevMonth.getFullYear(), lastDayPrevMonth.getMonth(), firstDayOnCal);  // set first date on Calendar in array
+            tmpDate = new Date(lastDayPrevMonth.getFullYear(), lastDayPrevMonth.getMonth(), firstDayOnCal);  // set first date on Calendar in array
+            if (dowFD == 1)
+              this.daysS[0][i].date = firstDateOnCalendar;
+            startDateForGettingData = this.daysS[0][i].date;
+            if (dowFD == 1 || dowFD == 6){
+              startDateForGettingData = firstDayOfShownMonth; 
+            }                                            
+            this.daysS[0][i].isInCurrMonth = tmpDate.getMonth() == monthShowNumber ? "inMonth" : "outMonth";
+          }
+          /***************    rest of days in first week *******************/
+          firstDayOnCal++; 
+          if (i > 0 )  {                            // go to next day
+            startDateForGettingData = this.daysS[0][i].date;
+            tmpDate =  new Date(this.daysS[0][i-1].date.getFullYear(), this.daysS[0][i-1].date.getMonth(), this.daysS[0][i-1].date.getDate()) // make a date to increment                                                                                           // from the previous entry in the loop
+            tmpDate.setDate(tmpDate.getDate() + 1);   
+                                          // increment the date
+            this.daysS[0][i].date = new Date(tmpDate.getFullYear(), tmpDate.getMonth(), tmpDate.getDate());                                                      // put that date in the dateBox of the MonthStructure
+            this.daysS[0][i].isInCurrMonth = tmpDate.getMonth() == monthShowNumber ? "inMonth" : "outMonth";
+          }
+          if (firstDayOnCal == lastDayNum + 1)                                                    // if it is greater than lastDayOfMonth
+            firstDayOnCal = 1;                                                                    // go to 1, for the first day of monthShown 
+            this.daysS[0][i].dateString = this.datePipe.transform(this.daysS[0][i].date, 'yyyy-MM-dd'); 
         }
-        /***************    rest of days in first week *******************/
-        firstDayOnCal++; 
-        if (i > 0 )  {                            // go to next day
-          startDateForGettingData = this.daysS[0][i].date;
-          tmpDate =  new Date(this.daysS[0][i-1].date.getFullYear(), this.daysS[0][i-1].date.getMonth(), this.daysS[0][i-1].date.getDate()) // make a date to increment                                                                                           // from the previous entry in the loop
-          tmpDate.setDate(tmpDate.getDate() + 1);   
-                                        // increment the date
-          this.daysS[0][i].date = new Date(tmpDate.getFullYear(), tmpDate.getMonth(), tmpDate.getDate());                                                      // put that date in the dateBox of the MonthStructure
-          this.daysS[0][i].isInCurrMonth = tmpDate.getMonth() == monthShowNumber ? "inMonth" : "outMonth";
-        }
-        if (firstDayOnCal == lastDayNum + 1)                                                    // if it is greater than lastDayOfMonth
-          firstDayOnCal = 1;                                                                    // go to 1, for the first day of monthShown 
-          this.daysS[0][i].dateString = this.datePipe.transform(this.daysS[0][i].date, 'yyyy-MM-dd');   }
+        GdayNumber = tmpDate.getDay()
+        console.log("at end of first GdayNumber is " + GdayNumber);  
 
-        }
-    if (dowFD == 1 || dowFD == 6){
-      firstDateOnCalendar.setDate(firstDateOnCalendar.getDate()- 1 ); 
-      tmpDate = firstDateOnCalendar  ;
-      startDateForGettingData = firstDayOfShownMonth;  
-    }  
+      }                                                                                 // end of first week generation
+      if (dowFD == 1 || dowFD == 6){
+          firstDateOnCalendar.setDate(firstDateOnCalendar.getDate()- 1 ); 
+          tmpDate = firstDateOnCalendar  ;
+          startDateForGettingData = firstDayOfShownMonth;  
+        }  
+
     if (dowFD == 0)
       tmpDate = firstDateOnCalendar;
     /*******   make the rest of the weeks on the calendar.  */  
     var firstTime = 0;
     for (let i=1; i < 6; i++){                                                              // max of 4 more weeks in calendar
       for (let j= 0; j < 5; j++) {                                                          // the days of each week
-
         let dayNum = tmpDate.getDay();                                                      // get dayNum of week, this will be Saturday 
-        console.log("242 tmpDate is %o dayNum is %o", tmpDate, dayNum)  
-        if (dayNum == 6 )                                                                   // check if it is Saturday,  
-          tmpDate.setDate(tmpDate.getDate() + 2);                                           // increment 2 days to get to Monday. 
-        if (dayNum == 5  && firstTime == 0)                                                                   // check if it is Friday 
+        if (dayNum == 5  && firstTime++ == 0 && tmpDate.getDate() < 6)  {                                             // check if it is Friday 
+          console.log("247 dayNum is %o tmpDate is %o", dayNum, tmpDate);
           tmpDate.setDate(tmpDate.getDate() + 3);                                           // increment 2 days to get to Monday.  
-        firstTime++;   
+
+        }
+
+        if (dayNum == 6 )    {                                                               // check if it is Saturday,  
+            tmpDate.setDate(tmpDate.getDate() + 2);                                           // increment 2 days to get to Monday. 
+        }
      //   else if (dayNum < 6 )
      //     tmpDate.setDate(tmpDate.getDate() + 1);                                             // increment the date
-        console.log("246 tmpDate is %o dayNum is %o", tmpDate, dayNum)  
+
         if (!this.daysS[i])                                                                 //  if array row has not been defined
             this.daysS[i] = Array();  
             this.daysS[i][j] = <dateBox>{};                                                 // define an instance of the daysS interface
@@ -256,10 +284,7 @@ export class MonthViewComponent implements OnInit {
             this.daysS[i][j].dateString = this.datePipe.transform(this.daysS[i][j].date, 'yyyy-MM-dd');
             this.daysS[i][j].dayNumber = tmpDate.getDate();
             this.daysS[i][j].isInCurrMonth = tmpDate.getMonth() == monthShowNumber ? "inMonth" : "outMonth";     
-          //  if (dayNum == 5 )                                                                   // check if it is Friday 
-            //  tmpDate.setDate(tmpDate.getDate() + 3);                                           // increment 2 days to get to Monday.   
-          // if (dayNum < 6 )
-              tmpDate.setDate(tmpDate.getDate() + 1);                                             // increment the date
+            tmpDate.setDate(tmpDate.getDate() + 1);                                             // increment the date
 
       }
     }
